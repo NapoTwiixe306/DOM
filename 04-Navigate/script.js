c6437e2250cad11c6b584a84ef7cc3546c06d37c
napotwiixe <julien.milants@gmail.com>
2023-06-10 19:31:36 +0200

    add 04

---
+++ 04-Navigate/script.js
@@ -0,0 +1,16 @@
+// Select the last child of the <ol> tag and put it at the beginning of the list
+var ol = document.querySelector('ol');
+var lastChild = ol.lastElementChild;
+ol.prepend(lastChild);
+
+// Move the <h2> of the third section in the second one and vice-versa
+var thirdSection = document.querySelectorAll('section')[2];
+var secondSection = document.querySelectorAll('section')[1];
+var h2ThirdSection = thirdSection.querySelector('h2');
+var h2SecondSection = secondSection.querySelector('h2');
+secondSection.insertBefore(h2ThirdSection, secondSection.firstChild);
+thirdSection.insertBefore(h2SecondSection, thirdSection.firstChild);
+
+// Delete the last section from the DOM
+var lastSection = document.querySelectorAll('section')[2];
+lastSection.parentNode.removeChild(lastSection);
